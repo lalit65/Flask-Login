c2591d00a3545d82373043b277f77020f03d6001
Herve Meftah <hmeftah@yahoo.com>
2017-11-04 15:45:37 +0100

    added closing function in signUp page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, json, request
 from flaskext.mysql import MySQL
 from flask_script import Manager
 from werkzeug import generate_password_hash, check_password_hash
+from contextlib import closing
 import os
 
 mysql = MySQL()
@@ -36,19 +37,17 @@ def signUp():
         # validate the received values
         if _name and _email and _password:
             
-            # All Good, let's call MySQL
-            
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-            data = cursor.fetchall()
+            with closing (mysql.connect()) as conn:
+                with closing(conn.cursor)) as cursor:
+                    _hashed_password = generate_password_hash(_password)
+                    cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+                    data = cursor.fetchall()
 
-            if len(data) is 0:
-                conn.commit()
-                return json.dumps({'message':'User created successfully !'})
-            else:
-                return json.dumps({'error':str(data[0])})
+                    if len(data) is 0:
+                        conn.commit()
+                        return json.dumps({'message':'User created successfully !'})
+                    else:
+                         return json.dumps({'error':str(data[0])})
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
 
